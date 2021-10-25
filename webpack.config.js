76741196e945400569d0c65b5760ac7c89a2bae7
SamuelV101 <91294247+samuelVF101@users.noreply.github.com>
2021-10-25 07:25:02 -0300

    Instalacion de Prettier
    Falta configurarlo

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-const path = require('path');
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const path = require("path");
 
 module.exports = {
-	...defaultConfig,
-	entry: {
-		index: path.resolve(process.cwd(), './app/js', 'index.js'),
-		customizer: path.resolve(process.cwd(), './app/js', 'customizer.js'),
-		navigation: path.resolve(process.cwd(), './app/js', 'navigation.js'),
-	},
-	output: {
-		filename: '[name].js',
-		path: path.resolve( __dirname, './build' ),
-	},
-	module: {
-		...defaultConfig.module,
-		rules: [
-			...defaultConfig.module.rules
-		],
-	},
+  ...defaultConfig,
+  entry: {
+    index: path.resolve(process.cwd(), "./app/js", "index.js"),
+    customizer: path.resolve(process.cwd(), "./app/js", "customizer.js"),
+    navigation: path.resolve(process.cwd(), "./app/js", "navigation.js"),
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./build"),
+  },
+  module: {
+    ...defaultConfig.module,
+    rules: [...defaultConfig.module.rules],
+  },
 };
